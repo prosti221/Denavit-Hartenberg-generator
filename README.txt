fd1a5906d4a43ec9119cdd4947dd68aa55661823
Prosti221 <faisalx12@gmail.com>
2021-04-10 13:37:06 +0200

    only using single function and passing var() for symbols

--- README.txt
+++ README.txt
@@ -4,5 +4,5 @@ This takes the following inputs:
     1. N number of links in configuration
     2. The DH parameters of the link: link length, link twist, link offset and joint angle.
 
-The output is the product of N homogeneous transformation matricies A_0 * A_1 ... A_N. The result can be presented numerically or algebraically.
+The output is the product of N homogeneous transformation matricies A_0 * A_1 ... A_N. 
 
--- main.py
+++ main.py
@@ -9,22 +9,16 @@ class Link:
         self.link_twist = link_twist
         self.link_offset = link_offset
         self.joint_angle = joint_angle
-        var('a_%d c_%d s_%d d_%d' % (self.link_num, self.link_num, self.link_num, self.link_num))
     
-    def compute_num_DH():
+    def compute_num_DH(self):
+        A = Matrix([[cos(self.joint_angle), -sin(self.joint_angle)*cos(self.link_twist) , sin(self.joint_angle)*sin(self.link_twist), self.link_length * cos(self.joint_angle)],  
+                    [sin(self.joint_angle), cos(self.joint_angle)*cos(self.link_twist) , -cos(self.joint_angle)*sin(self.link_twist), self.link_length * sin(self.joint_angle)],
+                    [0, sin(self.link_twist), cos(self.link_twist), self.link_offset],
+                    [0, 0, 0, 1]])
         return A
     
-    def compute_alg_DH(self):
-        if self.link_type == "revolute":
-            A = Matrix([[eval('c_%d'%self.link_num), -eval('s_%d'%self.link_num)*np.cos(self.link_twist), eval('s_%d'%self.link_num)*np.sin(self.link_twist), eval('a_%d*c_%d' % (self.link_num, self.link_num))], 
-                        [eval('s_%d'%self.link_num), eval('c_%d'%self.link_num)*np.cos(self.link_twist), -eval('c_%d'%self.link_num)*np.sin(self.link_twist), eval('a_%d*s_%d' % (self.link_num, self.link_num))], 
-                        [0, np.sin(self.link_twist), np.cos(self.link_twist), self.link_offset],
-                        [0, 0, 0, 1]])
-            return A
-        if self.link_type == "prismatic":
-            return A
-
-
 if __name__ == "__main__":
-    test = Link("revolute", 1, 0, -np.pi/2, var('L_1'), 0)
-    pprint(test.compute_alg_DH())
+    #Link(link_type, link_num, link_length, link_twist, link_offset, joint_angle)
+    #use var('x') for symbols
+    test = Link("prismatic", 1, 0, 0, var('a_5') + var('d_3'), 0)
+    pprint(test.compute_num_DH())
