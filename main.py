5974e1d0bbbc6b2e03b56e1bf2e8b3cd4a9b843c
Prosti221 <faisalx12@gmail.com>
2021-04-10 14:34:32 +0200

    Using only sympy instead of numpy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from sympy import *
-import numpy as np
 
 class Link:
     def __init__(self, link_type, link_num, link_length, link_twist, link_offset, joint_angle):
@@ -10,15 +9,37 @@ class Link:
         self.link_offset = link_offset
         self.joint_angle = joint_angle
     
-    def compute_num_DH(self):
+    def compute_num_DH(self, round=False):
         A = Matrix([[cos(self.joint_angle), -sin(self.joint_angle)*cos(self.link_twist) , sin(self.joint_angle)*sin(self.link_twist), self.link_length * cos(self.joint_angle)],  
                     [sin(self.joint_angle), cos(self.joint_angle)*cos(self.link_twist) , -cos(self.joint_angle)*sin(self.link_twist), self.link_length * sin(self.joint_angle)],
                     [0, sin(self.link_twist), cos(self.link_twist), self.link_offset],
                     [0, 0, 0, 1]])
+        if round: return self.round_matrix(A)
+        return A
+
+    def round_matrix(self, A):
+        for i in range(A.shape[0]):
+            for j in range(A.shape[1]):
+                if (isinstance(A[i,j], Float) and A[i,j] < 0.0000001):
+                    A[i,j] = 0
         return A
     
 if __name__ == "__main__":
-    #Link(link_type, link_num, link_length, link_twist, link_offset, joint_angle)
+    #Link(link_type, link_num(i), link_length(a), link_twist(alpha), link_offset(d), joint_angle(theta))
     #use var('x') for symbols
-    test = Link("prismatic", 1, 0, 0, var('a_5') + var('d_3'), 0)
-    pprint(test.compute_num_DH())
+    theta = '\u03B8'
+    link1 = Link("revolute", 1, 0, -pi/2, symbols('L_1'), symbols('%s_1' %(theta)))
+    link2 = Link("prismatic", 2, symbols('L_2'), 0, 0, symbols('%s_2' %(theta)))
+    link3 = Link("prismatic", 3, symbols('L_3'), 0, 0, symbols('%s_3' %(theta)))
+
+
+    A1 = link1.compute_num_DH(round=False)
+    A2 = link2.compute_num_DH(round=False)
+    A3 = link3.compute_num_DH(round=False)
+
+    pprint(A1)
+    pprint(A2)
+    pprint(A3)
+    pprint(A1*A2)
+    pprint(trigsimp(A1*A2*A3))
+
