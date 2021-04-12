5aedf3cb3ee424f1bed4872ed0b7008e265111fb
Prosti221 <faisalx12@gmail.com>
2021-04-12 23:27:14 +0200

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from sympy import *
-
-link_num = 2
-var('a_%d c_%d s_%d d_%d' % (link_num, link_num, link_num, link_num))
-A = Matrix([[eval('a_%d' % link_num), 2, 2], [2, 3, 4]])
-
-pprint(A)
