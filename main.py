1b10817486620cbe55afd5678b1de663560dd86d
Prosti221 <faisalx12@gmail.com>
2021-06-15 17:23:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -27,17 +27,13 @@ if __name__ == "__main__":
     #Dynamics
     D = 0   #Inertia matrix
     C = 0   #Coriolis/centrifugal matrix
-    g = 0   #gravity vector
     Ke = 0  #Total kinetic energy
-    Pe = 0  #Total potential energy
   
-    #links.append( Link(link_type="revolute", link_num=1, link_length=0, link_twist=-pi/2, link_offset=symbols('L_1'), joint_angle=symbols('%s_1' %(theta)) ) )
-    #links.append( Link(link_type="revolute", link_num=2, link_length=symbols('L_2'), link_twist=pi/2, link_offset=0, joint_angle=symbols('%s_2' %(theta))) )
-    #links.append( Link(link_type="prismatic", link_num=3, link_length=0, link_twist=0, link_offset=symbols('L_3'), joint_angle=0) )
-    
-    links.append( Link(link_type="prismatic", link_num=1, link_length=0, link_twist=0, link_offset=symbols('L_1'), joint_angle=0) )
-    links.append( Link(link_type="revolute", link_num=2, link_length=0, link_twist=0, link_offset=0, joint_angle=symbols('%s_2' %(theta))) )
-    
+    #Example configuration of a 3DOF RRP manipulator 
+    links.append( Link(link_type="revolute", link_num=1, link_length=0, link_twist=-pi/2, link_offset=symbols('L_1'), joint_angle=symbols('%s_1' %(theta)) ) )
+    links.append( Link(link_type="revolute", link_num=2, link_length=symbols('L_2'), link_twist=pi/2, link_offset=0, joint_angle=symbols('%s_2' %(theta))) )
+    links.append( Link(link_type="prismatic", link_num=3, link_length=0, link_twist=0, link_offset=symbols('L_3'), joint_angle=0) )
+        
     DOF = len(links)
 
     #Set the masses if needed
@@ -56,21 +52,19 @@ if __name__ == "__main__":
     
     T = eye(4)
 
-    print("\n\n######## TRANSFORMATION MATRICIES #########I\n\n")
-    for link in links: #print all the transformation matricies for each link
+    print("\n\n######## TRANSFORMATION MATRICIES #########\n\n")
+    for link in links:
         print("A_%d\n"%link.link_num)
         pprint(link.A)
-        print_simple(link.A, links)
         print_latex(link.A, links)
         
-    print("\n\n######## FORWARD KINEMATICS  #########I\n\n")
+    print("\n\n######## FORWARD KINEMATICS  #########\n\n")
     for link in links: 
         T = T * link.A
         for i in range(1, link.link_num + 1):
             print("A_%d"%i, end=" ")
         print("\n")
         pprint(trigsimp(T))
-        print_simple(trigsimp(T), links)
         print_latex(trigsimp(T), links)
   
     Jv = compute_linVel(links)
@@ -79,30 +73,23 @@ if __name__ == "__main__":
 
     print("############ ANGULAR VELOCITY #################\n\n")
     pprint(Jw)
-    print_simple(Jw, links)
     print_latex(Jw, links)
 
     print("############ LINEAR VELOCITY #################\n\n")
     pprint(Jv)
-    print_simple(Jv, links)
     print_latex(Jv, links)
     
     print("############ JACOBIAN #################\n\n")
     for i in range(1, 4):
         J = J.row_insert(3 + i, Jw.row(i-1))
     pprint(J)
-    print_simple(J, links)
     print_latex(J, links)
 
     print("############ Determinant of Jv  #################\n\n")
     #det = compute_singularities(Jv)
     #pprint(det)
-
-    #for link in links:
-    #    if link.link_type == "revolute":
-    #        link.joint_angle = dynamicsymbols('%s_%d'%(theta, link.link_num))
-
-    print("Ke: \n");
+    
+    #Dynamics
     D, Ke = compute_kinetic(J, links, DOF, q)
-    #C = compute_cristoffel(D, DOF, q)
+    C = compute_cristoffel(D, DOF, q)
 
